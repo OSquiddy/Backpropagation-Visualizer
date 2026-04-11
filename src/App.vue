7cf0abcd866da965b33019608afc70a5be12bf08
OSquiddy <siddiqui.omar21@gmail.com>
2026-04-11 00:30:29 -0500

    First commit of Backprop Visualizer V2

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import HeaderComponent from './components/HeaderComponent.vue'
+</script>
+
+<template>
+  <div class="container">
+    <HeaderComponent />
+    <h3>Perceptron</h3>
+    <p>A single layer perceptron is a simple neural network that can be used to classify data into two classes.</p>
+    
+    <h3>Multi-Layer Perceptron</h3>
+    <p>A multi-layer perceptron is a neural network that can be used to classify data into multiple classes.</p>
+    <h3>Deep Neural Network</h3>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  padding: 15px 30px;
+  background-color: #f5f5f5;
+
+  h3 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #333;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+}
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1>Backpropagation Visualizer</h1>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  font-weight: 600;
+  color: #333;
+}
+</style>
