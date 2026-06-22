5c5c397594e5d745719318641f6edccaf85e4f80
OSquiddy <siddiqui.omar21@gmail.com>
2026-06-21 21:40:04 -0500

    Updated Tensor type definitions

--- src/types/Tensor.type.ts
+++ src/types/Tensor.type.ts
@@ -1,60 +1,33 @@
-export type Tensor =
-  | {
-      id: string
-      type: 'input' | 'weight' | 'bias'
-      incomingValue: number | null
-      outgoingValue: number | null
-      value: number
-      parents: string[]
-      children: string[]
-      gradient: number | null | undefined
-      label: string
-      layerId: number
-    }
-  | {
-      id: string
-      type: 'relu' | 'sigmoid' | 'tanh' | 'softmax' | 'linear'
-      incomingValue: number
-      outgoingValue: number
-      parents: string[]
-      children: string[]
-      gradient: number | number[]
-      label: string
-      layerId: number
-    }
-  | {
-      id: string
-      type: 'add' | 'subtract' | 'multiply' | 'divide' | 'dot' | 'matmul' | 'sum'
-      incomingValue: number[] | number
-      outgoingValue: number
-      parents: string[]
-      children: string[]
-      gradient: number | number[]
-      label: string
-      layerId: number
-    }
-  | {
-      id: string
-      type: 'cross_entropy' | 'mean_squared_error' | 'mean_absolute_error'
-      incomingValue: number[] | number
-      outgoingValue: number
-      parents: string[]
-      children: string[]
-      gradient: number | number[]
-      label: string
-      layerId: number
-    }
+import type { NDArrayCore, DType } from 'numpy-ts/core'
 
-export type ValueTypeTensors = Extract<Tensor, { type: 'input' | 'weight' | 'bias' }>
-export type LossTypeTensors = Extract<
-  Tensor,
-  { type: 'cross_entropy' | 'mean_squared_error' | 'mean_absolute_error' }
->
-export type OperationTypeTensors = Extract<
-  Tensor,
-  { type: 'add' | 'subtract' | 'multiply' | 'divide' | 'dot' | 'matmul' | 'sum' }
->
-export type ActivationTypeTensors = Extract<
-  Tensor,
-  { type: 'relu' | 'sigmoid' | 'tanh' | 'softmax' | 'linear' }
->
+export type Tensor = number | Tensor[] | DType | NDArrayCore
+
+export type BaseTensorType = {
+  id: string
+  parents: { id: string, type: string }[]
+  children: { id: string, type: string }[]
+  label: string
+  layerId: number
+  gradient?: Tensor,
+  incomingValue: Tensor
+  outgoingValue: Tensor
+}
+
+export type ValueTypeTensors = BaseTensorType & {
+  type: 'input' | 'weight' | 'bias' | 'ground-truth' | 'loss_output'
+  value: Tensor
+}
+
+export type LossTypeTensors = BaseTensorType & {
+  type: 'cross_entropy' | 'mean_squared_error' | 'mean_absolute_error' | 'hinge_loss' | 'binary_cross_entropy'
+}
+
+export type OperationTypeTensors = BaseTensorType & {
+  type: 'add' | 'subtract' | 'multiply' | 'divide' | 'dot' | 'matmul' | 'sum'
+}
+
+export type ActivationTypeTensors = BaseTensorType & {
+  type: 'relu' | 'sigmoid' | 'tanh' | 'softmax' | 'linear'
+}
+
+export type TensorType = ValueTypeTensors | LossTypeTensors | OperationTypeTensors | ActivationTypeTensors
