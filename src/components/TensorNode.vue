446a80f378b1e738f4244557666a8628b2b5deb9
OSquiddy <siddiqui.omar21@gmail.com>
2026-06-29 17:40:43 -0500

    Got rid of unused code from TensorNode.vue

--- src/components/TensorNode.vue
+++ src/components/TensorNode.vue
@@ -3,8 +3,7 @@ import { Handle, Position } from '@vue-flow/core'
 import { computed } from 'vue'
 import { useTensorDataStore } from '@/stores/tensorDataStore'
 import { storeToRefs } from 'pinia'
-import type { OperationTypeTensors, LossTypeTensors, ActivationTypeTensors, ValueTypeTensors } from '@/types/Tensor.type'
-import type { LayerPayload } from '@/types/Layer.type'
+import type { ValueTypeTensors } from '@/types/Tensor.type'
 import { ltx } from '@/utils/LaTeXFormatter'
 
 const props = defineProps<{
@@ -20,81 +19,6 @@ const { tensorValuesMap } = storeToRefs(tensorValuesStore)
 const currentNode = computed(() => {
   return tensorValuesMap.value[props.id]
 })
-
-
-const layerType = computed(() => {
-  const type = currentNode.value!.type
-  if (type == 'cross_entropy' || type == 'mean_squared_error' || type == 'mean_absolute_error') {
-    return 'loss'
-  }
-  if (type == 'relu' || type == 'sigmoid' || type == 'tanh' || type == 'softmax' || type == 'linear') {
-    return 'activation'
-  }
-  if (type == 'add' || type == 'subtract' || type == 'multiply' || type == 'divide' || type == 'dot' || type == 'matmul' || type == 'sum') {
-    return 'operation'
-  }
-  if (type == 'input' || type == 'weight' || type == 'bias') {
-    return 'data'
-  }
-  return type
-})
-
-const layerSubType = computed(() => {
-  const type = currentNode.value!.type
-  switch (type) {
-    case 'input':
-      return 'inputs'
-    case 'weight':
-      return 'weights'
-    case 'bias':
-      return 'biases'
-    case 'loss_output':
-      return 'loss_output'
-    default:
-      return type
-  }
-})
-
-let layerDataPayload: LayerPayload | undefined = undefined
-const node = currentNode.value
-
-layerDataPayload = {
-  id: node!.layerId,
-  type: layerType.value,
-  prev: node!.layerId - 1,
-  next: node!.layerId + 1,
-}
-
-const commonTensorData = {
-  id: props.id,
-  type: node!.type,
-  incomingValue: node!.incomingValue,
-  parents: node!.parents,
-  children: node!.children,
-  label: node!.label,
-  layerId: node!.layerId,
-}
-
-if (layerType.value === 'data') {
-  const subType = layerSubType.value as 'inputs' | 'weights' | 'biases'
-  layerDataPayload[subType] = {
-    ...commonTensorData,
-    value: (node as ValueTypeTensors).value,
-    outgoingValue: 0,
-    gradient: 0,
-    incomingGradient: 0,
-    localGradient: 0,
-  } as ValueTypeTensors
-} else {
-  layerDataPayload.data = {
-    ...commonTensorData,
-    outgoingValue: 0,
-    gradient: 0,
-    incomingGradient: 0,
-    localGradient: 0,
-  } as OperationTypeTensors | LossTypeTensors | ActivationTypeTensors
-}
-
 
 const formattedLabel = computed(() => {
   if (currentNode.value!.label) {
